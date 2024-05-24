40cdcd6038fde2505c2e32b982c51022e765cec0
Danil Semenchin <semen4in.d@gmail.com>
2024-05-24 11:00:08 +0300

    Создание проекта под будущую разработку

---
+++ src/main/java/ru/semyak/buysell/BuySellApplication.java
@@ -0,0 +1,13 @@
+package ru.semyak.buysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySellApplication.class, args);
+	}
+
+}
